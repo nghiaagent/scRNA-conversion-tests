1a56a8de47fe3dc89022eb9a9e4fc1118f097737
Nghia Nguyen <nghia6a1@gmail.com>
2021-08-25 15:38:16 +1000

    Reorganised code, enabled download.

--- draft.R
+++ draft.R
@@ -1,11 +1,9 @@
 #### CAREFUL ####
 
-### First, please put your .h5ad file in ./data ###
-
-### /CAREFUL ###
+# First, please put your .h5ad file in ./
+# Or download the example dataset.
 
 #### Grab packages ####
-
 if (!require(pacman)) {
   install.packages(pacman)
 }
@@ -30,19 +28,17 @@ if (!requireNamespace("iSEE",
 }
 library(iSEE)
 
+#### Download dataset from humancellatlas.usegalaxy.eu ####
+download.file(url = "https://humancellatlas.usegalaxy.eu/datasets/11ac94870d0bb33a6e276e5f70445ab0/display?to_ext=h5ad",
+              destfile = "./mice_scRNA.h5ad")
 
-#### OPTIONAL: Download dataset from humancellatlas.usegalaxy.eu ####
-
-# download.file(url = "https://humancellatlas.usegalaxy.eu/datasets/11ac94870d0bb33a6e276e5f70445ab0/display?to_ext=h5ad",
-#               destfile = "./data/mice_scRNA.h5ad")
-
-#### Import data ####
-
-scRNA_object_example <- readH5AD(file = "./data/mice_scRNA.h5ad") ### PCA, tSNE already calculated and embedded in dataset
-
-# scRNA_object_mine <- readH5AD(file = "./data/microglia.h5ad") ### PCA, tSNE already calculated and embedded in dataset
-
+#### Example dataset ####
+scRNA_object_example <- readH5AD(file = "./mice_scRNA.h5ad") ### PCA, tSNE already calculated and embedded in dataset
 vis_example <- iSEE(scRNA_object_example)
+shiny::runApp(vis_example)
+
+#### Published dataset ####
+# scRNA_object_mine <- readH5AD(file = "./data/microglia.h5ad") ### PCA, tSNE already calculated and embedded in dataset
 # vis_mine <- iSEE(scRNA_object_mine)
 
-shiny::runApp(vis_example)
+
